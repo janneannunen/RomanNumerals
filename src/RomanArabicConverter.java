5501668b45c500a2df50aa4efcf70c13833bfdfc
janneannunen <jannunen@student.oulu.fi>
2018-10-11 20:43:43 +0300

    The solution provided gives me errors, not fault as I wished, but detects the faults and the error is shown as failure trace note

--- src/RomanArabicConverter.java
+++ src/RomanArabicConverter.java
@@ -1,8 +1,20 @@
 import java.util.List;
-
+/*
+ * Rules:
+There	were	certain	rules	that	the	numerals	followed	which	should	be	observed:
+1. The	symbols	'I',	'X',	'C',	and	'M'	can	be	repeated	at	most	3	times	in	a row.
+2. The	symbols	'V',	'L',	and	'D'	can	never	be repeated.
+3. The '1' symbols ('I', 'X', and 'C') can only be subtracted from the 2 next highest values ('IV' and	
+'IX',	'XL'	and	'XC',	'CD'	and 'CM').
+4. Only	one	subtraction	can	be	made	per	numeral	('XC'	is	allowed,	'XXC'	is not).
+5. The	'5'	symbols	('V',	'L',	and	'D')	can	never	be subtracted
+ */
 public class RomanArabicConverter {
 	public static int romanToArabic(String input) {
         String romanNumeral = input.toUpperCase();
+        String romanNumeralCheck="null";
+        String romanErrorType="null";
+        		
         int result = 0;
         
         List<RomanNumeral> romanNumerals = RomanNumeral.getReverseSortedValues();
@@ -19,10 +31,47 @@ public class RomanArabicConverter {
             else {
                 i++;
             }
-        }
+            
+            /*The solution provided gives me errors, not fault as I wished, 
+            * but detects the faults and the error is shown as failure trace note
+            */
+            
+            romanNumeralCheck = romanNumeral;
+            romanErrorType="IIII";
+        
+            if (romanNumeralCheck == romanErrorType){ 	
+            	throw new IllegalArgumentException(romanNumeralCheck + " more than 3 I in a row");	
+            }
+            romanErrorType="XXXX";
+            if (romanNumeralCheck == "XXXX"){ 	
+            	throw new IllegalArgumentException(input + " more than 3 X in a row");	
+            }
+            romanErrorType="CCCC";
+            if (romanNumeralCheck == "CCCC"){ 	
+            	throw new IllegalArgumentException(input + " more than 3 C in a row");	
+            }
+            romanErrorType="MMMM";
+            if (romanNumeralCheck == "MMMM"){ 	
+            	throw new IllegalArgumentException(input + " more than 3 M in a row");	
+            }
+            romanErrorType="VV";
+            if (romanNumeralCheck == "VV"){ 	
+            	throw new IllegalArgumentException(input + " more than 2 V in a row");	
+            }
+            romanErrorType="LL";
+            if (romanNumeralCheck == "LL"){ 	
+            	throw new IllegalArgumentException(input + " more than 2 L in a row");	
+            }
+            romanErrorType="DD";
+            if (romanNumeralCheck == romanErrorType){ 	
+            	throw new IllegalArgumentException(input + " more than 2 D in a row");	
+            }
+        } 
+        
         if (romanNumeral.length() > 0) {
             throw new IllegalArgumentException(input + " cannot be converted to a Roman Numeral");
         }
+             
         
         return result;
     }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -14,6 +14,7 @@ There	were	certain	rules	that	the	numerals	followed	which	should	be	observed:
 'IX',	'XL'	and	'XC',	'CD'	and 'CM').
 4. Only	one	subtraction	can	be	made	per	numeral	('XC'	is	allowed,	'XXC'	is not).
 5. The	'5'	symbols	('V',	'L',	and	'D')	can	never	be subtracted
+
  */
 
 import static org.junit.Assert.*;
@@ -69,10 +70,8 @@ public class TestRomanNumerals {
 	@Test
 	public void ShouldFail_IIII() {
 
-		String roman = "IIII";
-
+		String roman = "IIII"; // IV correct
 		int result = RomanArabicConverter.romanToArabic(roman);
-
 		assertEquals(result, 4);
 
 	}
@@ -80,7 +79,7 @@ public class TestRomanNumerals {
 	@Test
 	public void ShouldFail_VV() {
 
-		String roman = "VV";
+		String roman = "VV"; // X is the right format 
 
 		int result = RomanArabicConverter.romanToArabic(roman);
 
@@ -90,9 +89,9 @@ public class TestRomanNumerals {
 	@Test
 	public void ShouldFail_IC() {
 
-		String roman = "IC";
+		String roman = "IC";// XCIX
 
-		int result = RomanArabicConverter.romanToArabic(roman);
+		int result = RomanArabicConverter.romanToArabic(roman); 
 
 		assertEquals(result, 99);
 
@@ -100,23 +99,52 @@ public class TestRomanNumerals {
 	@Test
 	public void ShouldFail_IIX() {
 
-		String roman = "IIX";
-
+		String roman = "IIX"; //Should Fail VIII is correct format
 		int result = RomanArabicConverter.romanToArabic(roman);
-
 		assertEquals(result, 8);
 
 	}
+	
 	@Test
-	public void ShouldFailRule5() {
+	public void ShouldFailRule_VX() {
 
-		String roman = "VX";
+		String roman = "VX"; //V
 
 		int result = RomanArabicConverter.romanToArabic(roman);
 
 		assertEquals(result, 5);
-
 	}
+	
+	@Test
+	public void ShouldFailRule_XXC() {
 
+		String roman = "XXC"; //80
 
+		int result = RomanArabicConverter.romanToArabic(roman);
+
+		assertEquals(result, 80);
+	}
+	@Test
+	public void ShouldFailRule_IM() {
+
+		String roman = "IM"; //999
+
+		int result = RomanArabicConverter.romanToArabic(roman);
+
+		assertEquals(result, 999);
+	}
+/*	@Test 
+ * I could not get this to work. The solution provided gives me errors, not fault as I wished, 
+ * but detects the faults and the error is shown as failure trace note
+ * 
+	public void ShouldFailRule_VX() throws IllegalArgumentException {
+
+		String roman = "VX"; //V
+
+                // Expects IllegalArgumentException to be thrown in the future...
+                thrown.expect(java.lang.IllegalArgumentException.class);
+		int result = RomanArabicConverter.romanToArabic(roman);
+                // The execution doesn't really get here...
+		assertEquals(result, 5);
+	}*/
 }
